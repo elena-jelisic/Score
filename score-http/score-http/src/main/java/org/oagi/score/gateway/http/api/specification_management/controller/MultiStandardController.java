bdf1d865463e11a96223dfcef0d14b7aac14ee6e
elena-jelisic <71283987+elena-jelisic@users.noreply.github.com>
2024-04-03 00:54:54 -0400

    Specification controller

--- score-http/score-http/src/main/java/org/oagi/score/gateway/http/api/specification_management/controller/MultiStandardController.java
+++ score-http/score-http/src/main/java/org/oagi/score/gateway/http/api/specification_management/controller/MultiStandardController.java
@@ -1,8 +1,13 @@
 package org.oagi.score.gateway.http.api.specification_management.controller;
 
+import org.oagi.score.gateway.http.api.bie_management.data.BieCreateRequest;
 import org.oagi.score.gateway.http.api.specification_management.service.MultiStandardService;
+import org.oagi.score.repo.api.user.model.ScoreUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticatedPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +18,11 @@ public class MultiStandardController {
     @Autowired
     MultiStandardService service;
 
-    @RequestMapping(value = "/import_specification", method = RequestMethod.POST,
+    @RequestMapping(value = "/import_specification", method = RequestMethod.PUT,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public String getSimpleNamespaces() {
-        service.insertNewSpecification();
+    public String insertNewSpecification(@AuthenticationPrincipal AuthenticatedPrincipal user,
+                                         @RequestBody BieCreateRequest bieCreateRequest) {
+        service.insertNewSpecification(user);
         return "Success";
     }
 
